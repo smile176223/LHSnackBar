007bdee621971df54b312feea1ff936ffcc6adb6
LiamHsu <s459958@gmail.com>
2024-03-14 16:44:10 +0800

    Create framework project

---
+++ LHSnackBar/LHSnackBar/LHSnackBar.h
@@ -0,0 +1,18 @@
+//
+//  LHSnackBar.h
+//  LHSnackBar
+//
+//  Created by Liam on 2024/3/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LHSnackBar.
+FOUNDATION_EXPORT double LHSnackBarVersionNumber;
+
+//! Project version string for LHSnackBar.
+FOUNDATION_EXPORT const unsigned char LHSnackBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LHSnackBar/PublicHeader.h>
+
+
---
+++ LHSnackBar/LHSnackBar/SnackBar.swift
@@ -0,0 +1,18 @@
+//
+//  SnackBar.swift
+//  LHSnackBar
+//
+//  Created by Liam on 2024/3/14.
+//
+
+import SwiftUI
+
+struct SnackBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SnackBar()
+}
